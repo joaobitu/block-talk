22b63086e10d439e0e58b9fc73fe99727b476a05
Joao <joaovbitu@gmail.com>
2022-11-27 10:58:05 -0300

    firebase configured, firestore database configured

--- src/App.js
+++ src/App.js
@@ -2,8 +2,29 @@ import "./style.css";
 import { Sidebar } from "./components/Sidebar";
 import { Header } from "./components/Header";
 import { Description } from "./components/Description";
+import { useEffect, useState } from "react";
+import { db } from "./firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
+  const [blocks, setBlocks] = useState([]);
+  const blocksCollectionRef = collection(db, "blocks");
+
+  const getBlocksList = async () => {
+    const data = await getDocs(blocksCollectionRef);
+    setBlocks(
+      data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+        user: doc.user,
+      }))
+    );
+  };
+
+  useEffect(() => {
+    getBlocksList();
+  }, []);
+
   return (
     <div className="App">
       <Header />
---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyDijHtmpN8LcSUke1jwOAgZNlpmhLRtzf4",
+  authDomain: "block-talk-42c0d.firebaseapp.com",
+  projectId: "block-talk-42c0d",
+  storageBucket: "block-talk-42c0d.appspot.com",
+  messagingSenderId: "487535311993",
+  appId: "1:487535311993:web:24a912c46a1cf85c4968a8",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
